ff6518f43692b025ac0c93920aa01f2a8292f751
Terramind App Builder <app-builder@terramind.com>
2025-08-05 15:34:54 +0000

    Auto-fix: Resolved 1 errors in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   experimental: {
-    serverComponentsExternalPackages: []
+    appDir: true,
   },
-  env: {
-    PORT: '4000'
-  }
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
